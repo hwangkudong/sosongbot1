0f7100317b26936469059d56f54eb66f74872700
kd h <attain2004@daum.net>
2017-04-09 20:17:30 -0500

    ddd

--- router/main.js
+++ router/main.js
@@ -49,54 +49,25 @@ module.exports = function(app, fs)
 			})
 		}else { // 아닐 경우 이미지검색 실시
 			var request = require('request'); //get방식으로 request에 대한 response를 받기위해
-			var encodeURISafe = require('encodeuri-safe'); //get방식에 쓰일 파라미터 값을 인코딩하기 위해
-			var param = encodeURISafe.encodeURIComponent(req.body["content"]);
-			var options = {
-			  url: 'https://openapi.naver.com/v1/search/image.xml?query='+param+'&start=1&display=1',
-			  headers: {
-				'User-Agent': 'curl/7.43.0',
-				'X-Naver-Client-Id': 'wzBUyhB6kx0L3nIbKZHO', //네이버 OpenAPI에서 발급받은 Client-Id 삽입
-				'X-Naver-Client-Secret': 'ZVF5BnZzae' //네이버 OpenAPI에서 발급받은 Client-Secret 삽입
-			  }
-			};
-						
-			function callback(error, response, body) {
-				// 에러 체크
-				if(error) return console.log('Error:', error);
-				// 상태 값 체크
-				if(response.statusCode !== 200)	return console.log('Invalid Status Code Returned:', response.statusCode);
-				
-				// 반환 값에서 이미지 url만 추출
-				body = body.substring(body.indexOf("<thumbnail>")+11, body.indexOf("</thumbnail>"));
-				console.log('test'+body);
-				var bodyTemp = body;
-				body = 	{
-							"photo": {
-								"url": body,
-								"width": 640,
-								"height": 480
-							}
-						};
-				
-				// 파일 입출력
-				fs.readFile( __dirname + "/../data/messageImg.json", 'utf8',  function(err, data){
-					var messages = JSON.parse(data);
-					// 이미지검색 결과 저장 및 번환
-					messages["message"] = body;
-					fs.writeFile(__dirname + "/../data/messageImg.json",
-								 JSON.stringify(messages, null, '\t'), "utf8", function(err, data){
-					})
-					fs.readFile( __dirname + "/../data/messageImg.json", 'utf8', function (err, data) {
-						// 결과 로그 출력
-						console.log("Request_user_key : "+req.body["user_key"]);
-						console.log("ImgURL : "+bodyTemp);
-						res.end(data);
-						return;
-					})
-				})
-			}
+			var watson = require('watson-developer-cloud');
+
+			var natural_language_classifier = watson.natural_language_classifier({
+			  username: 'd76902b6-f214-4a16-9ff7-0db1a6de9888',
+			  password: 'YjyQG68nMzHo',
+			  version: 'v1'
+			});
 			
-			request(options, callback);
+			natural_language_classifier.classify({
+			  text: 'How hot will it be today?',
+			  classifier_id: '90e7b4x199-nlc-36160' },
+			  function(err, response) {
+			    if (err)
+			      console.log('error:', err);
+			    else{
+			      console.log(JSON.stringify(response, null, 2));
+			      }
+			});
+			
 		}
     });
 	
