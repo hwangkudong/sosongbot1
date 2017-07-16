53fb91964abceb8198c00bda46a6c6711d9948f0
이뿐이양지 <@218.147.163.33>
2017-07-16 17:36:17 +0900

    NLC ID수정

--- router/main.js
+++ router/main.js
@@ -64,7 +64,7 @@ module.exports = function(app, fs)
 			
 			natural_language_classifier.classify({
 				  text: inputText,
-				  classifier_id: '90e7b4x199-nlc-36441' },
+				  classifier_id: '359f41x201-nlc-197451' },
 				  function(err, response) {
 				    if (err)
 				      console.log('error:', err);
