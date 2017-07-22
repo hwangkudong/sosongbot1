0d8aa15f0d1e058077885ea40a420f6929c59b79
hkd <hkd@121.133.228.161>
2017-07-22 15:45:46 +0900

    tn수정

--- router/main.js
+++ router/main.js
@@ -74,7 +74,9 @@ module.exports = function(app, fs)
 				      console.log("결과 : " + result1);
 				      
 				      var result2 = JSON.parse(result1);
-				      console.log("결과2 : " + result2);
+				      var result3 = result2["class_name"]
+				      
+				      console.log("결과3 : " + result3);
 				      
 				      fs.readFile( __dirname + "/../data/message.json", 'utf8',  function(err, data){
 							var messages = JSON.parse(data);
