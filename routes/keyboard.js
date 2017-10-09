4aa371040f933cb2a335102411810d2ceaaaa675
yoonjho <yoonjho79@gmail.com>
2017-10-09 23:48:01 +0900

    keyboard  수정중

--- routes/keyboard.js
+++ routes/keyboard.js
@@ -2,47 +2,52 @@ var fs = require('fs');
 
 exports.keyboard = function(req, res, sosongbotUHD) {
 
-    console.log('keyboard.js..............................');
+    console.log('/keyboard logic is starting..');
 
     sosongbotUHD.find({menu_nm:'HOME'}, function(err, sosongbotUHD) {
-     
+
       if(err) {
         return res.status(500).send({error:'no data'});
       } else {
 
         fs.readFile( __dirname + "/../message/keyboard.json", 'utf8',  function(err, data){
-          
-          if ( err) { 
-            
-          } else {
 
-            var messages = JSON.parse(data);
-            var m_button  = sosongbotUHD[0].button;
-            var marr = sosongbotUHD[0].button.split(",");
-            messages = {"type" : "buttons", "buttons" :  marr  };
-            res.json(messages);
+          if ( err) {
+            console.log('error is ' + err);
+          } else {
+            try{
+              var messages = JSON.parse(data);
+              var m_button  = sosongbotUHD[0].button;
+              var marr = sosongbotUHD[0].button.split(",");
+              messages = {"type" : "buttons", "buttons" :  marr  };
+              res.json(messages);
+            }catch(except){
+              console.log('exception is ' + except);
+              var messages = {"type" : "text", "buttons" :  marr  };//todo: 에러 메시지로 수정
+              res.json(messages);
+            }
 
           }
-        });       
+        });
       }
-    }); 
+    });
 };
 
 exports.keyboardFromMessage = function(req, res, sosongbotUHD) {
 
     var v_menu = '';
-    
+
     if ( req.body['content'] === "" ) {
         v_menu = 'HOME';
     } else {
         v_menu = req.body['content'];
-        
+
     }
-    
+
     //console.log('선택한 메뉴 값:' + v_menu);
-    
+
     sosongbotUHD.find({menu_nm:v_menu}, function(err, sosongbotUHD) {
-     
+
       if(err) {
         return res.status(500).send({error:'no data'});
       } else {
@@ -51,23 +56,21 @@ exports.keyboardFromMessage = function(req, res, sosongbotUHD) {
         //console.log('내용:' + sosongbotUHD[0].content);
 
         fs.readFile( __dirname + "/../message/msgkeyboard.json", 'utf8',  function(err, data){
-          
-          if ( err) { 
-            
+
+          if ( err) {
+
           } else {
-            
+
             var messages = JSON.parse(data);
-            
+
             var m_button  = sosongbotUHD[0].button;
             messages["message"] = {"text" : sosongbotUHD[0].content };
             var marr = sosongbotUHD[0].button.split(",");
-            messages["keyboard"] = {"type" : "buttons", "buttons" :  marr  }; 
-            
+            messages["keyboard"] = {"type" : "buttons", "buttons" :  marr  };
+
             res.json(messages);
           }
-        });       
+        });
       }
     });
 }
-  
-
